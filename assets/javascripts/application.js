eed3498ac25ec985b672ae53d36ee87f554a9951
Denis Yagofarov <denyago@gmail.com>
2014-08-26 02:01:25 +0300

    Deck.js update to 1.1.0 and small textual amendments

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,9 +1,9 @@
 //= require modernizr.custom
 //= require jquery
 //= require deck/deck.core.js
-//= require deck/extensions/deck.hash.js
 //= require deck/extensions/deck.navigation.js
 //= require deck/extensions/deck.status.js
+//= require deck/extensions/deck.scale.js
 //= require syntax-highlighter/XRegExp.js
 //= require syntax-highlighter/shCore.js
 //= require syntax-highlighter/shBrushPlain.js
